10a0a6dfb437679c2c96ee8b3034fa499828eb7d
narone <buzzin2426@gmail.com>
2025-04-17 18:35:58 +0900

    refactor: UserRegisterRequest의 오탈자 수정

--- src/main/java/team03/monew/dto/user/UserRegisterRequest.java
+++ src/main/java/team03/monew/dto/user/UserRegisterRequest.java
@@ -18,7 +18,7 @@ public record UserRegisterRequest(
     String nickname,
 
     @NotBlank(message = "비밀번호는 필수입니다")
-    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 60자 이하여야 합니다")
+    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 100자 이하여야 합니다")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,}$",
         message = "비밀번호는 최소 8자 이상, 숫자, 문자, 특수문자를 포함해야 합니다")
     String password
