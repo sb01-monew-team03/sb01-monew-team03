b4aa3d3a8a3ac77aa64248b1c542bbbc6607d9a0
hyanyul <tjdwl0808@gmail.com>
2025-04-17 18:15:24 +0900

    feat : Add validation for interest dtos

--- src/main/java/team03/monew/dto/interest/InterestRegisterRequest.java
+++ src/main/java/team03/monew/dto/interest/InterestRegisterRequest.java
@@ -1,10 +1,17 @@
 package team03.monew.dto.interest;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record InterestRegisterRequest(
+    @NotBlank
+    @Size(max = 50, message = "관심사는 50자 이하로 입력해주세요.")
     String name,
-    List<String> keywords
+
+    @NotNull
+    List<@Size(max = 20, message = "키워드는 20자 이하로 입력해주세요.") String> keywords
 ) {
 
 }
--- src/main/java/team03/monew/dto/interest/InterestUpdateRequest.java
+++ src/main/java/team03/monew/dto/interest/InterestUpdateRequest.java
@@ -1,9 +1,12 @@
 package team03.monew.dto.interest;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record InterestUpdateRequest(
-    List<String> keywords
+    @NotNull
+    List<@Size(max = 20, message = "키워드는 20자 이하로 입력해주세요.") String> keywords
 ) {
 
 }
