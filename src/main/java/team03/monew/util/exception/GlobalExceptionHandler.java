1972dc0c0d2a6335ac59aa98a7f567261468be03
narone <buzzin2426@gmail.com>
2025-04-18 15:30:47 +0900

    feature: exception 구현

--- src/main/java/team03/monew/util/exception/GlobalExceptionHandler.java
+++ src/main/java/team03/monew/util/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package team03.monew.util.exception;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
