95e80019f4a93b2dcffd4a1ee37494ee907b6390
narone <buzzin2426@gmail.com>
2025-04-18 16:32:26 +0900

    refactor: ErrorCode 코드네임 수정

--- src/main/java/team03/monew/util/exception/ErrorCode.java
+++ src/main/java/team03/monew/util/exception/ErrorCode.java
@@ -9,11 +9,13 @@ public enum ErrorCode {
   // user 에러 코드
   USER_NOT_FOUND("USER_001", "User not found", HttpStatus.NOT_FOUND),
   INVALID_USER_CREDENTIALS("USER_002", "Wrong password", HttpStatus.UNAUTHORIZED),
-  DUPLICATE_USER("USER_002", "Duplicate user", HttpStatus.BAD_REQUEST),
+  DUPLICATE_USER("USER_003", "Duplicate user", HttpStatus.BAD_REQUEST),
 
   // Server 에러 코드
   INTERNAL_SERVER_ERROR("SERVER_001","서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-  INVALID_REQUEST("SERVER+002", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+
+  // Request 에러 코드
+  INVALID_REQUEST("REQUEST_001", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
 
   private final String code;
   private final String message;
