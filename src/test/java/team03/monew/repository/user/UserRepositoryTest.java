39efeb7462e350bbe33ca85bcfd4b56bd27e1829
narone <buzzin2426@gmail.com>
2025-04-21 15:26:27 +0900

    feat: UserRepositoryTest 작성 및 UserUpdateRequest 구현

---
+++ src/test/java/team03/monew/repository/user/UserRepositoryTest.java
@@ -0,0 +1,98 @@
+package team03.monew.repository.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.persistence.EntityManager;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
+import team03.monew.entity.user.User;
+
+@DataJpaTest
+@EnableJpaAuditing
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private EntityManager entityManager;
+
+  @Nested
+  @DisplayName("이메일로 사용자 여부 검색")
+  class existsByEmail {
+
+    @Test
+    @DisplayName("이메일로 사용자를 찾았을 경우")
+    void existsByEmail_success() {
+      // given
+      String email = "user@gmail.com";
+      User user = new User("user", email, "qwer1234");
+      userRepository.save(user);
+      entityManager.flush();
+      entityManager.clear();
+
+      // when
+      boolean exists = userRepository.existsByEmail(email);
+
+      // then
+      assertTrue(exists);
+    }
+
+    @Test
+    @DisplayName("해당 이메일의 사용자가 없을 경우")
+    void existsByEmail_returnFalse() {
+      // given
+      String email = "user@gmail.com";
+
+      // when
+      boolean exists = userRepository.existsByEmail(email);
+
+      // then
+      assertFalse(exists);
+    }
+  }
+
+  @Nested
+  @DisplayName("이메일로 사용자 찾기")
+  class findByEmail {
+
+    @Test
+    @DisplayName("사용자 찾기 성공")
+    void findByEmail_success() {
+      // given
+      String email = "user@gmail.com";
+      User user = new User("user", email, "qwer1234");
+      userRepository.save(user);
+      entityManager.flush();
+      entityManager.clear();
+
+      // when
+      Optional<User> result = userRepository.findByEmail(email);
+
+      // then
+      assertThat(result).isPresent();
+      assertEquals(result.get().getNickname(), user.getNickname());
+    }
+
+    @Test
+    @DisplayName("사용자 찾기 실패")
+    void findByEmail_fail() {
+      // given
+      String email = "user@gmail.com";
+
+      // when
+      Optional<User> result = userRepository.findByEmail(email);
+
+      // then
+      assertThat(result).isEmpty();
+    }
+  }
+}
