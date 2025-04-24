573a8cf11d1d8b2ce091fa7f34fcfebb92635425
narone <buzzin2426@gmail.com>
2025-04-24 18:08:50 +0900

    feat: User 관련 권한 관리 인터셉터 구현

--- src/main/java/team03/monew/controller/user/UserController.java
+++ src/main/java/team03/monew/controller/user/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team03.monew.dto.user.UserDto;
@@ -36,6 +37,7 @@ public class UserController {
 
   @PatchMapping("/{userId}")
   public ResponseEntity<UserDto> update(@PathVariable UUID userId,
+      @RequestHeader("MoNew-Request-User-ID") UUID requestUserId,
       @RequestBody @Valid UserUpdateRequest request) {
     log.info("사용자 수정 요청: userId={}, request={}", userId, request);
     UserDto userDto = userService.update(userId, request);
@@ -44,14 +46,16 @@ public class UserController {
   }
 
   @DeleteMapping("/{userId}")
-  public ResponseEntity<Void> softDelete(@PathVariable UUID userId) {
+  public ResponseEntity<Void> softDelete(@PathVariable UUID userId,
+      @RequestHeader("MoNew-Request-User-ID") UUID requestUserId) {
     log.info("사용자 논리 삭제 요청: userId={}", userId);
     userService.softDelete(userId);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
   @DeleteMapping("/{userId}/hard")
-  public ResponseEntity<Void> hardDelete(@PathVariable UUID userId) {
+  public ResponseEntity<Void> hardDelete(@PathVariable UUID userId,
+      @RequestHeader("MoNew-Request-User-ID") UUID requestUserId) {
     log.info("사용자 물리 삭제 요청: userId={}", userId);
     userService.hardDelete(userId);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
