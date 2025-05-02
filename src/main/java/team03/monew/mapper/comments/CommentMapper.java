687a3a8547d37668950ab95dc0f7ab9a0c985d2d
doil1130 <kbkg1138@gmail.com>
2025-05-02 10:37:03 +0900

    refactor: commentRepository 수정

--- src/main/java/team03/monew/mapper/comments/CommentMapper.java
+++ src/main/java/team03/monew/mapper/comments/CommentMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import team03.monew.dto.comments.CommentDto;
 import team03.monew.dto.comments.CommentLikeDto;
+import team03.monew.dto.comments.CommentActivityDto;
 import team03.monew.entity.comments.Comment;
 import team03.monew.entity.comments.CommentLike;
 
@@ -34,4 +35,12 @@ public interface CommentMapper {
     @Mapping(target = "commentLikeCount", source = "like.comment.likeCount")
     @Mapping(target = "commentCreatedAt", source = "like.comment.createdAt")
     CommentLikeDto toLikeDto(CommentLike like);
+
+    @Mapping(target = "id", source = "comment.id")
+    @Mapping(target = "articleId", source = "comment.article.id")
+    @Mapping(target = "userId", source = "comment.user.id")
+    @Mapping(target = "userNickname", source = "comment.user.nickname")
+    @Mapping(target = "content", source = "comment.content")
+    @Mapping(target = "createdAt", source = "comment.createdAt")
+    CommentActivityDto toActivityDto(Comment comment);
 }
--- src/main/java/team03/monew/repository/comments/CommentRepository.java
+++ src/main/java/team03/monew/repository/comments/CommentRepository.java
@@ -1,5 +1,6 @@
 package team03.monew.repository.comments;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
@@ -7,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import team03.monew.entity.article.Article;
+import team03.monew.entity.user.User;
 import team03.monew.entity.comments.Comment;
 
 @Repository
@@ -15,6 +18,9 @@ public interface CommentRepository
         JpaSpecificationExecutor<Comment> {
 
     Page<Comment> findByArticleIdAndDeletedAtIsNull(UUID articleId, Pageable pageable);
-
     Optional<Comment> findByIdAndUserId(UUID commentId, UUID userId);
+
+    List<Comment> findTop10ByUserOrderByCreatedAtDesc(User user);
+
+    long countByArticle(Article article);
 }
